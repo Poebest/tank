1ceac67fc38c9033366beff8aa639411ce7972f9
poe <poebest@163.com>
2020-06-20 10:55:32 +0800

    使用线程池创建线程

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,14 @@
 package main.java;
 
+import cn.hutool.core.thread.ThreadFactoryBuilder;
 import lombok.extern.slf4j.Slf4j;
 import main.java.com.poe.tank.common.Constants;
 import main.java.com.poe.tank.entity.Tank;
 import main.java.com.poe.tank.entity.TankFrame;
 import main.java.com.poe.tank.enums.Dir;
 import main.java.com.poe.tank.enums.Group;
+
+import java.util.concurrent.*;
 
 /**
  * @version v1.0
@@ -23,15 +26,36 @@ public class Main {
             //初始化敌方tank
             tf.tanks.add(new Tank(100 + i * 80, 200, Dir.DOWN, Group.BAD, tf));
         }
-        new Thread(() -> {
+//        new Thread(() -> {
+//            while (true) {
+//                try {
+//                    Thread.sleep(50);
+//                } catch (InterruptedException e) {
+//                    log.error("tank error", e);
+//                }
+//                tf.repaint();
+//            }
+//        }).start();
+
+
+
+        ThreadFactory namedThreadFactory = new ThreadFactoryBuilder()
+                .setNamePrefix("tank Thread").build();
+        ExecutorService singleThreadPool = new ThreadPoolExecutor(1, 4,
+                0L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<Runnable>(1024), namedThreadFactory, new ThreadPoolExecutor.AbortPolicy());
+
+        singleThreadPool.execute(()-> {
             while (true) {
                 try {
                     Thread.sleep(50);
                 } catch (InterruptedException e) {
                     log.error("tank error", e);
                 }
+//                log.info("线程名称 : {} ", Thread.currentThread().getName());
                 tf.repaint();
             }
-        }).start();
+        });
+        singleThreadPool.shutdown();
     }
 }
